990611b7c9f511d26c1083688bb85b4c3a9951e2
HarpLoid <walesii19@gmail.com>
2023-06-08 20:14:44 +0100

    initial commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    arg_count = len(argv)
+    num_of_args = arg_count - 1
+
+    if (arg_count == 1):
+        print("{} arguments.".format(num_of_args))
+    elif (arg_count > 1):
+        print("{} arguments:".format(num_of_args))
+    else:
+        print("{} argument".format(num_of_args))
+
+    for i in range(1, arg_count):
+        print("{}: {}".format(i, argv[i]))
