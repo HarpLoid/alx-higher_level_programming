d796d051e5deb05b83771a118b89359814ed6266
HarpLoid <walesii19@gmail.com>
2023-06-09 08:59:28 +0100

    new commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,8 +10,9 @@ if __name__ == "__main__":
         print("{} arguments.".format(num_of_args))
     elif (arg_count > 1):
         print("{} arguments:".format(num_of_args))
+        for i in range(1, arg_count):
+            print("{}: {}".format(i, argv[i]))
     else:
         print("{} argument".format(num_of_args))
+        print("{}: {}".format(i, argv[num_of_args]))
 
-    for i in range(1, arg_count):
-        print("{}: {}".format(i, argv[i]))
