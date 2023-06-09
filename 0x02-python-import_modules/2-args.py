e7df4f418c025fb948a737b170b1c0edde4ed7ea
HarpLoid <walesii19@gmail.com>
2023-06-09 09:23:59 +0100

    fixed bug with 1 argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,12 +6,12 @@ if __name__ == "__main__":
     arg_count = len(argv)
     num_of_args = arg_count - 1
 
-    if (arg_count == 1):
+    if (num_of_args == 0):
         print("{} arguments.".format(num_of_args))
-    elif (arg_count > 1):
+    elif (num_of_args > 1):
         print("{} arguments:".format(num_of_args))
         for i in range(1, arg_count):
             print("{}: {}".format(i, argv[i]))
     else:
-        print("{} argument".format(num_of_args))
-        print("{}: {}".format(i, argv[num_of_args]))
+        print("{} argument:".format(num_of_args))
+        print("{}: {}".format(num_of_args, argv[num_of_args]))
