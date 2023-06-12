04a307ad9cba7d8b26ccda9d9b211693bfceaab2
HarpLoid <walesii19@gmail.com>
2023-06-12 01:32:51 +0100

    add condition for null list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,13 +2,11 @@
 
 def max_integer(my_list=[]):
 
-    max = 0
-
-    if len(my_list) == 0:
+    if my_list:
+        max = 0
+        for i in my_list:
+            if i > max:
+                max = i
+        return (max)
+    else:
         return None
-
-    for i in my_list:
-        if i > max:
-            max = i
-
-    return (max)
