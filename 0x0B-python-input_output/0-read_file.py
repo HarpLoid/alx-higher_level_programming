f997f1706427763507495abcc9b8530c0aea35bc
Harploid <walesii19@gmail.com>
2023-07-12 09:06:41 +0000

    read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module - 0-read_file
+"""
+
+
+def read_file(filename=""):
+	""" Reads content of a file """
+	with open(filename, encoding="utf-8") as f:
+		read_content = f.read()
+	print(read_content)
