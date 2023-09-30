2626bf997c08ec605841a067216e44eeb14bde43
HarpLoid <walesii19@gmail.com>
2023-09-30 11:31:08 +0100

    search api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+Takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user
+with the letter 'q' as a parameter.
+"""
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(argv) > 1:
+        q = argv[1]
+    else:
+        q = ""
+
+    values = {"q": q}
+    resp = requests.post(url, data=values)
+    try:
+        val_json = resp.json()
+        if val_json:
+            print(f"[{val_json['id']}] {val_json['name']}")
+        else:
+            print("No result")
+    except requests.exceptions.InvalidJSONError:
+        print("Not a valid JSON")
