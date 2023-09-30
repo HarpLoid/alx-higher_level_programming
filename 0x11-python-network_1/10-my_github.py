f83f887e9ec83463679f763b2179886b3c5bac45
HarpLoid <walesii19@gmail.com>
2023-09-30 13:23:18 +0100

    github api

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+Takes your GitHub credentials (username and password) 
+and uses the GitHub API to display your id
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    user = argv[1]
+    token = argv[2]
+    url = "https://api.github.com/user"
+    headers = {'Authorization': f'Bearer {token}'}
+
+    try:
+        resp = requests.get(url, headers=headers)
+        resp.raise_for_status()
+        print(resp.json()["id"])
+    except requests.exceptions.HTTPError:
+        print("None")
+        exit()
