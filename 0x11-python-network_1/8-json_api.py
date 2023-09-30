90596783984bfc88b15e82fa74c6c1edbc2e962e
HarpLoid <walesii19@gmail.com>
2023-09-30 11:49:55 +0100

    debugging not a JSON error

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,7 +8,7 @@ import requests
 from sys import argv
 
 if __name__ == '__main__':
-    url = 'http://0.0.0.0:5000/search_user'
+    url = 'http://736def419d41.6e431165.alx-cod.online:5000/search_user'
     if len(argv) > 1:
         q = argv[1]
     else:
@@ -18,9 +18,10 @@ if __name__ == '__main__':
     resp = requests.post(url, data=values)
     try:
         val_json = resp.json()
+        print(val_json)
         if val_json:
             print(f"[{val_json['id']}] {val_json['name']}")
         else:
             print("No result")
-    except requests.exceptions.InvalidJSONError:
+    except requests.exceptions.JSONDecodeError:
         print("Not a valid JSON")
