f28e6422f38b932ae70428a26a9be7fb31a9c4fc
HarpLoid <walesii19@gmail.com>
2023-10-24 23:44:49 +0100

    fixed style error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 
 const fs = require('fs');
 
-let file_path = process.argv[2];
+const filePath = process.argv[2];
 
-fs.readFile(file_path, 'utf8', function (error, body) {
+fs.readFile(filePath, 'utf8', function (error, body) {
   if (error) {
     console.log(error);
   } else {
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,10 +2,10 @@
 
 const fs = require('fs');
 
-let file_path = process.argv[2];
-let content = process.argv[3];
+const filePath = process.argv[2];
+const content = process.argv[3];
 
-fs.writeFile(file_path, content, 'utf8', (error) => {
+fs.writeFile(filePath, content, 'utf8', (error) => {
   if (error) {
     console.log(error);
   }
