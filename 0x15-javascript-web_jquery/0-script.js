12c82701a807979f8a6bd8ebef0db32dc117a061
HarpLoid <walesii19@gmail.com>
2023-11-02 13:39:36 +0100

    Web dynamic with JS and JQuery

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,9 @@
+/* Updates the text color of
+the header element to red */
+
+function changeTextColor () {
+  const header = document.querySelector('header');
+  header.style.color = '#FF0000';
+}
+
+changeTextColor();
---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,10 @@
+/**
+ * updates the text color of the <header> element
+ * to red (#FF0000)
+ */
+
+function headerColor () {
+  $('header').css('color', '#FF0000');
+}
+
+$(document).ready(headerColor);
